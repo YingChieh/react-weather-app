b2748dc22b8383359a5933ffe62f6b15ef5966f0
Ying-Chieh Chen <lycajdl@gmail.com>
2021-10-26 15:52:14 +0200

    get weather by current location

--- src/App.js
+++ src/App.js
@@ -5,45 +5,6 @@ import Weather from "./components/Weather";
 import Feature from "./components/Feature";
 import { removeAlert } from "./code";
 
-// To get the weather for a particular location
-async function getLocationWeather(location) {
-  // API KEY AND URL
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const apiUrl =
-    process.env.REACT_APP_API_URL +
-    `/weather?q=${location}&appid=${apiKey}&exclude=current&units=metric`;
-
-  try {
-    const result = await fetch(apiUrl);
-
-    // the API call has succeeded
-    switch (result.status) {
-      case 200:
-        return { success: true, data: await result.json() };
-      case 401:
-        return {
-          success: false,
-          error: "Your API key is not activated",
-        };
-      case 404:
-        return {
-          success: false,
-          error: "Your searched a wrong city name",
-        };
-      case 429:
-        return {
-          success: false,
-          error:
-            "Please consider upgrading to a subscription plan that meets your needs or reduce the number of API calls in accordance with the limits. (less than 60 API calls per minute)",
-        };
-      default:
-        return { success: false, error: result.statusText };
-    }
-  } catch (ex) {
-    return { success: false, error: ex.message };
-  }
-}
-
 function App() {
   let initLocation = "taipei";
   if (localStorage.getItem("location") !== null) {
@@ -52,56 +13,105 @@ function App() {
   // State
   const [apiData, setApiData] = useState({}); // to store the response
   const [inputState, setInputState] = useState(initLocation); // to store the location name from input field
-  const [state, setState] = useState(initLocation); // to store a copy of the getState this will be helpful in updating state on button click
+
+  // API KEY AND URL
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const url = process.env.REACT_APP_API_URL;
+  const [apiUrl, setApiUrl] = useState(
+    `${url}/weather?q=${initLocation}&appid=${apiKey}&exclude=current&units=metric`
+  );
+
   const inputHandler = (event) => {
     setInputState(event.target.value);
   };
   // to copy the state from inputState to state
   const submitHandler = () => {
-    setState(inputState);
+    setApiUrl(
+      `${url}/weather?q=${inputState}&appid=${apiKey}&exclude=current&units=metric`
+    );
+  };
 
-    // Store
-    if (localStorage.getItem("location") === null) {
-      localStorage.setItem("location", inputState);
-    } else {
-      localStorage.removeItem("location");
-      localStorage.setItem("location", inputState);
+  const currentLocation = () => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      let lat = position.coords.latitude;
+      let long = position.coords.longitude;
+      setApiUrl(
+        `${url}/weather?lat=${lat}&lon=${long}&appid=${apiKey}&exclude=current&units=metric`
+      );
+    });
+  };
+
+  useEffect(() => {
+    // To get the weather for a particular location
+    async function getLocationWeather() {
+      try {
+        const result = await fetch(apiUrl);
+
+        // the API call has succeeded
+        switch (result.status) {
+          case 200:
+            return { success: true, data: await result.json() };
+          case 401:
+            return {
+              success: false,
+              error: "Your API key is not activated",
+            };
+          case 404:
+            return {
+              success: false,
+              error: "Your searched a wrong city name",
+            };
+          case 429:
+            return {
+              success: false,
+              error:
+                "Please consider upgrading to a subscription plan that meets your needs or reduce the number of API calls in accordance with the limits. (less than 60 API calls per minute)",
+            };
+          default:
+            return { success: false, error: result.statusText };
+        }
+      } catch (ex) {
+        return { success: false, error: ex.message };
+      }
     }
-  };
-
-  // Retrieving the Data
-  const getWeather = async () => {
-    const result = await getLocationWeather(state);
-    setApiData(result);
-    console.log(result.data);
-  };
-  useEffect(() => {
+    // Retrieving the Data
+    const getWeather = async () => {
+      const result = await getLocationWeather();
+      setApiData(result);
+      var inputEle = document.getElementsByClassName("searchbar")[0];
+      inputEle.value = result.data.name;
+      // Store
+      if (localStorage.getItem("location") === null) {
+        localStorage.setItem("location", result.data.name);
+      } else {
+        localStorage.removeItem("location");
+        localStorage.setItem("location", result.data.name);
+      }
+    };
     getWeather();
-  }, []);
-  useEffect(() => {
     // make the weather to be updated every 10 seconds
     var handle = setInterval(getWeather, 120000);
 
     return () => {
       clearInterval(handle);
     };
-  });
+  }, [apiUrl]);
 
   return (
     <>
       <div className="App">
-        <form>
+        <div>
           <input
             type="search"
             className="searchbar transparent"
             placeholder="Enter City..."
             onChange={inputHandler}
           />
-          <i className="fas fa-map-marker-alt"></i>
+          <i className="fas fa-map-marker-alt" onClick={currentLocation}></i>
           <button className="search-btn transparent" onClick={submitHandler}>
             GO
           </button>
-        </form>
+        </div>
         {apiData.success ? (
           <div className="content">
             <Weather weatherData={apiData.data}></Weather>
